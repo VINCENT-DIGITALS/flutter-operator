7e6b58ecb89393f1c554775b7cb99afd80f0dae5
VINCENT-DIGITALS <johnvincentmacayananpro@gmail.com>
2024-09-14 12:00:21 +0800

    changed

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get_storage/get_storage.dart';
 import 'firebase_options.dart';
 
-
 void main() async {
   await dotenv.load(fileName: '.env');
   WidgetsFlutterBinding. ensureInitialized();
